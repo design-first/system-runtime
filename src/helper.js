eb2d5eeded126b83476ca15d21781c15c4f83068
Erwan Carriou <erwan.carriou@mac.com>
2018-09-07 15:03:17 +0200

    generated uuid are now valid GUID

--- src/helper.js
+++ src/helper.js
@@ -107,16 +107,22 @@ exports.getRequire = function getRequire() {
  * @description Generate a uuid
  */
 exports.generateId = function generateId() {
+  // taken from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
   function gen() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16);
+    return 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0;
+      var v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
   }
 
+  // force the uuid to start with a letter
   function getPrefix() {
-    var validPrefix = 'abcdefghijklmnopqrstuvwxyz';
+    var validPrefix = 'abcdef';
     return validPrefix.charAt(Math.floor(Math.random() * validPrefix.length));
   }
 
-  return getPrefix() + gen() + gen() + gen();
+  return getPrefix() + gen();
 };
 
 /**
